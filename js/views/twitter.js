f3cbe2b6d427b46508745b6ffcb055f93535e811
garbagemanonfire <nilesvm@gmail.com>
2014-02-07 21:33:55 -0800

    cleaned up layout and navigation

--- js/views/twitter.js
+++ js/views/twitter.js
@@ -19,7 +19,6 @@ function initialize(app) {
 
 function render() {
   this.$el.html(this.template());
-
 };
 
 function tweetBulid(model) {
@@ -29,6 +28,5 @@ function tweetBulid(model) {
 };
 
 function onTweet() {
-
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");  
 };
